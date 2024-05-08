fe88add525fda0c225c341190bf8cb20a8178788
George Makura <makurag@proton.me>
2024-05-08 11:29:27 +0200

    Add isTrap() and printBoard() methods to the Board class

--- src/main/java/arimaa/game/Board.java
+++ src/main/java/arimaa/game/Board.java
@@ -3,8 +3,8 @@ package src.main.java.arimaa.game;
 import src.main.java.arimaa.pieces.Piece;
 
 public class Board {
-    private final int BOARD_SIZE = 8;
-    private final Piece[][] grid;
+    private static final int SIZE = 8; // Arimaa has an 8x8 board
+    private Piece[][] grid = new Piece[SIZE][SIZE];
     // Example: Trap positions (c3, c6, f3, f6)
     private final Position[] traps = {
             new Position("c3"),
@@ -14,7 +14,7 @@ public class Board {
     };
 
     public Board() {
-        grid = new Piece[BOARD_SIZE][BOARD_SIZE];
+
     }
 
     // Places a piece at the given position
@@ -37,5 +37,38 @@ public class Board {
             return true;
         }
         return false;
+    }
+
+    public boolean isTrap(Position position) {
+        for (Position trap : traps) {
+            if (trap.equals(position)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void printBoard() {
+        System.out.println("  +-----------------+");
+
+        for (int row = SIZE; row > 0; row--) {
+            System.out.print(row + " | ");
+            for (int col = 0; col < SIZE; col++) {
+                Piece piece = grid[col][row - 1];
+                Position currentPosition = new Position((char) ('a' + col), row);
+
+                if (piece != null) {
+                    System.out.print(piece.getType() + " ");
+                } else if (isTrap(currentPosition)) {
+                    System.out.print("x ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println("|");
+        }
+
+        System.out.println("  +-----------------+");
+        System.out.println("    a b c d e f g h");
     }
 }
