34298d44cb29f6f360a5d3e0b2cec6dc922f4294
George Makura <makurag@proton.me>
2024-05-15 12:21:31 +0200

    Initialize Main class

---
+++ src/main/java/arimaa/Main.java
@@ -0,0 +1,9 @@
+package src.main.java.arimaa;
+
+import src.main.java.arimaa.gui.HomePage;
+
+public class Main {
+    public static void main(String[] args) {
+        HomePage homePage = new HomePage();
+    }
+}
